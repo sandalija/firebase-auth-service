0057ade398d452d0510e7a415c10a5f160c5cf89
sandalija <42692586+sandalija@users.noreply.github.com>
2022-03-28 09:49:52 +0200

    Draft: Create + edit + delete users (#1)
    * WIP
    
    * CRUD user
    
    * Clean up

--- server.ts
+++ server.ts
@@ -1,12 +1,37 @@
 import express, { Request, Response }  from "express"
+import { createUserRoute, deleteUserRoute, readUserRoute, updateUserRoute } from "./src/user-managment/crud";
+import * as firebase from 'firebase-admin';
+import serviceAccount from "./service-account";
+import * as sa from "./firebase-adminsdk.json"
 
+
+firebase.initializeApp({credential: firebase.credential.cert({...sa } as any),})
 //Create an app
-const app = express();
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world\n');
-});
 
-//Listen port
-const PORT = 8080;
-app.listen(PORT);
-console.log(`Running on port ${PORT}`);
+try {
+    const app = express();
+    app.use(express.json());
+    app.use((err: unknown, req: Request, res: Response, next: Function) => {
+        console.error(err)
+        next()
+    })
+    app.post('/user', (req, res) => createUserRoute(req, res));
+    app.get('/user/:uid', (req, res) => readUserRoute(req, res));
+    app.patch('/user/:uid', (req, res) => updateUserRoute(req, res));
+    app.delete('/user/:uid', (req, res) => deleteUserRoute(req, res));
+
+
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Hello world\n');
+    });
+
+    //Listen port
+    const PORT = 8080;
+    app.listen(PORT);
+    console.log(`Running on port ${PORT}`);
+} catch(e) {
+    console.error(e)
+}
+
+
+
---
+++ src/user-managment/crud.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express"
+import { getAuth } from "firebase-admin/auth"
+
+export const createUserRoute = async (req: Request, res: Response) => {
+  try {
+    const result = await getAuth().createUser(req.body as any)
+    res.send(result)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
+}
+
+export const readUserRoute = async (req: Request, res: Response) => {
+  try {
+    const uid : string = req.params.uid.toString();
+    const result = await getAuth().getUser(uid)
+    res.send(result)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
+}
+
+export const updateUserRoute = async (req: Request, res: Response) => {
+  try {
+    const uid : string = req.params.uid.toString();
+    const result = await getAuth().updateUser(uid, req.body as any)
+    res.send(result)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
+}
+
+export const deleteUserRoute = async (req: Request, res: Response) => {
+  try {
+    const uid : string = req.params.uid.toString();
+    const result = await getAuth().deleteUser(uid)
+    res.send(result)
+  } catch (e) {
+    console.error(e)
+    res.status(500).send(e)
+  }
+}
+
